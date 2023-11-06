943cda6cf467dd32fe84811882df80ed417631dd
YounYoung Heo <112863029+Heo-y-y@users.noreply.github.com>
2023-11-06 22:35:24 +0900

    [Bronze V] Title: A+B - 5, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,27 +1,22 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringBuilder sb = new StringBuilder();
 
         while (true) {
-
             String str = br.readLine();
+            int a = str.charAt(0) - 48;
+            int b = str.charAt(2) - 48;
 
-            int A = str.charAt(0) - 48;
-            int B = str.charAt(2) - 48;
-
-            if (A == 0 && B == 0) {
+            if (a == 0 && b == 0) {
                 break;
             }
-            sb.append((A + B)).append('\n');
+            sb.append(a + b).append('\n');
         }
         System.out.println(sb);
     }
 }
-
