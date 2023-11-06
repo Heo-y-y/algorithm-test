9c637bcfcb865d588359930aa0cf204177f7f58b
YounYoung Heo <112863029+Heo-y-y@users.noreply.github.com>
2023-11-06 22:46:13 +0900

    [Bronze V] Title: A×B, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        String str = bufferedReader.readLine();
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
         StringTokenizer st = new StringTokenizer(str, " ");
 
         int a = Integer.parseInt(st.nextToken());
